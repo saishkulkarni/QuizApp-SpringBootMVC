15ef4e4732be8b12705cf3b0a879ffda5a6a3d64
saishkulkarni <saishkulkarni7@gmail.com>
2023-10-14 11:24:28 +0530

    Project Created:wq

---
+++ src/main/java/org/jsp/quiz/QuizAppApplication.java
@@ -0,0 +1,13 @@
+package org.jsp.quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jsp/quiz/QuizAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jsp.quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
