b2d9a07575fb481250a6143389a1926e5ec52711
saishkulkarni <saishkulkarni7@gmail.com>
2023-10-16 13:53:21 +0530

    Student validation done

--- src/main/java/org/jsp/quiz/controller/StudentController.java
+++ src/main/java/org/jsp/quiz/controller/StudentController.java
@@ -1,19 +1,40 @@
 package org.jsp.quiz.controller;
 
+import org.jsp.quiz.dto.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
+
+	@Autowired
+	Student student;
+
 	@GetMapping("/login")
 	public String loadLogin() {
 		return "StudentLogin";
 	}
 
 	@GetMapping("/signup")
-	public String loadSignup() {
+	public String loadSignup(ModelMap map) {
+		map.put("student", student);
 		return "StudentSignup";
+	}
+
+	@PostMapping("/signup")
+	public String signup(@Valid Student student, BindingResult result, ModelMap map) {
+		if (result.hasErrors()) {
+			return "StudentSignup";
+		} else {
+			return "StudentLogin";
+		}
 	}
 }
--- src/main/java/org/jsp/quiz/dto/Student.java
+++ src/main/java/org/jsp/quiz/dto/Student.java
@@ -1,17 +1,24 @@
 package org.jsp.quiz.dto;
 
+import org.springframework.stereotype.Component;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Entity
 @Data
+@Component
 public class Student {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,11 +26,16 @@ public class Student {
 	@Size(min = 3, message = "*Enter Atleast 3 charecters")
 	@Size(max = 15, message = "*At Max only 15 charecters Allowed")
 	private String name;
-	@Email(message = "Enter Proper Email")
-	@NotEmpty
+	@Email(message = "*Enter Proper Email")
+	@NotEmpty(message = "*Enter Proper Email")
 	private String email;
+	@DecimalMin(value = "6000000000", message = "*Enter Proper Mobile Number")
+	@DecimalMax(value = "9999999999", message = "*Enter Proper Mobile Number")
+	@NotNull(message = "*Enter Proper Mobile Number")
 	private long mobile;
+	@Pattern(regexp = "^.*(?=.{8,})(?=..*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$", message = "*Enter atleast one uppercase,lowercase,digit and special charecter and minimum 8 charecters")
 	private String password;
+	@NotEmpty(message = "*This Field Can not be Empty")
 	private String gender;
 	@Lob
 	private byte[] picture;
